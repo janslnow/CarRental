ef665cd46f1a9f8e7f2ca865d4f91f466be14782
jiangjie <@>
2022-09-10 23:22:57 +0800

    2022.09.10 optimize code;

--- src/main/java/com/demo/rental/service/CarService.java
+++ src/main/java/com/demo/rental/service/CarService.java
@@ -32,32 +32,42 @@ public class CarService {
 
         List<CarVO> result = new ArrayList<>();
         for (Map.Entry<Integer, List<CarAndRentalInfo>> entry : groupByCarId.entrySet()) {
-
-            Integer carId = entry.getKey();
-            List<CarAndRentalInfo> reservedCarDateList = entry.getValue();
-            String carModel = reservedCarDateList.get(0).getCarModel();
-            List<CarReservedDateVO> carReservedDateVOList =
-                    reservedCarDateList
-                            .stream()
-                            .filter(carAndRentalInfo -> carAndRentalInfo.getRentalStartDate() != null)
-                            .map(carAndRentalInfo -> {
-                                CarReservedDateVO carReservedDateVO = new CarReservedDateVO();
-                                carReservedDateVO.setRentalStartDate(DateUtil.formatDate(carAndRentalInfo.getRentalStartDate(), DateUtil.DEFAULT_DATE_STYLE));
-                                carReservedDateVO.setRentalEndDate(DateUtil.formatDate(carAndRentalInfo.getRentalEndDate(), DateUtil.DEFAULT_DATE_STYLE));
-
-                                return carReservedDateVO;
-                            })
-                            .collect(Collectors.toList());
-
-            CarVO carVO = new CarVO();
-            carVO.setCarId(carId);
-            carVO.setCarModel(carModel);
-            carVO.setReservedDatePeriod(carReservedDateVOList);
-
-            result.add(carVO);
+            result.add(transferToCarVO(entry));
         }
 
         return result;
+    }
+
+    private CarVO transferToCarVO(Map.Entry<Integer, List<CarAndRentalInfo>> entry) {
+
+        Integer carId = entry.getKey();
+        List<CarAndRentalInfo> reservedCarDateList = entry.getValue();
+        String carModel = reservedCarDateList.get(0).getCarModel();
+        List<CarReservedDateVO> carReservedDateVOList = transferToCarReservedDateVO(reservedCarDateList);
+
+        CarVO carVO = new CarVO();
+        carVO.setCarId(carId);
+        carVO.setCarModel(carModel);
+        carVO.setReservedDatePeriod(carReservedDateVOList);
+
+        return carVO;
+    }
+
+    private List<CarReservedDateVO> transferToCarReservedDateVO(List<CarAndRentalInfo> reservedCarDateList) {
+
+        return reservedCarDateList
+                .stream()
+                .filter(carAndRentalInfo -> carAndRentalInfo.getRentalStartDate() != null)
+                .map(carAndRentalInfo -> {
+
+                    CarReservedDateVO carReservedDateVO = new CarReservedDateVO();
+                    carReservedDateVO.setRentalStartDate(DateUtil.formatDate(carAndRentalInfo.getRentalStartDate(), DateUtil.DEFAULT_DATE_STYLE));
+                    carReservedDateVO.setRentalEndDate(DateUtil.formatDate(carAndRentalInfo.getRentalEndDate(), DateUtil.DEFAULT_DATE_STYLE));
+
+                    return carReservedDateVO;
+                })
+                .collect(Collectors.toList());
+
     }
 
 }
